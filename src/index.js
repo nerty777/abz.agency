f9fe7a7c556609f7f3239818a17b6f9b7ee0baa3
Ivan Kozhyn <nerty777@gmail.com>
2021-11-14 08:35:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import store from './Store/Store';
+import store from './store/store';
 import App from './components/App/App';
 
 import './index.scss';
